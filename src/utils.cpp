6999d69e9eccd8f2d36796aaf9399b963e3cb854
MeshariAlsh <meshari.alsh21@gmail.com>
2024-11-11 00:38:40 +0000

    Declared constant values at the top

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,12 +3,16 @@
 #include <iostream>
 #include <vector>
 
+constexpr float X_OFFSET = 170.0f;
+constexpr float Y_OFFSET = 70.0f;
+constexpr float CELL_SIZE = 15.0f;
+
 // Compute the row and column based on the cell position
 namespace utils 
 {
     void MouseToCellCoords(sf::Vector2f& cellPosition, int& row, int& column)
     {
-    row = (cellPosition.x - 170.0f) / 15.0f;
-    column = (cellPosition.y - 70.0f) / 15.0f;
+    row = (cellPosition.x - X_OFFSET) / CELL_SIZE;
+    column = (cellPosition.y - Y_OFFSET) / CELL_SIZE;
     }
 }
